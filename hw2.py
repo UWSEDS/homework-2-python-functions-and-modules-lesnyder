b1cea8bb9409859ed9a3c09e0f85eb6e4486903f
lesnyder <snyder.lauren.elizabeth@gmail.com>
2019-11-14 08:56:07 -0800

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,17 @@
+"""Create the function test_create_dataframe that takes as input: (a) a pandas DataFrame and (b) a list of column names. The function returns True if the following conditions hold:
+
+	The DataFrame contains only the columns that you specified as the second argument.
+	The values in each column have the same python type
+	There are at least 10 rows in the DataFrame."""
+
+import pandas as pd
+
+link =  https://data.seattle.gov/api/views/tw7j-dfaw/rows.csv?accessType=DOWNLOAD
+df = pd.read_csv(link) 
+
+def test_create_dataframe(df, column_names):
+       return column_names== list (df.column)
+
+for col in df: 
+	for i  in range (len(df)): 
+	(type(df[col][0]!==type 
